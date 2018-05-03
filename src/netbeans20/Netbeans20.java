ac4ff841edb26bcf4cb70287bd5ff1ee45dee46e
madrid <madrid@EQUIPOBRYAN>
2018-05-03 12:15:01 +0200

    Prueba 1

---
+++ src/netbeans20/Netbeans20.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeans20;
+
+/**
+ *
+ * @author madrid
+ */
+public class Netbeans20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //comentario
+        System.out.println("AdroNabo");
+    }
+    
+}
